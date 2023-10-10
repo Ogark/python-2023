4fb349fc751bbb53d829170c27df84be5abc899a
Ogark <ogark012@gmail.com>
2023-10-10 23:55:48 +0300

    Up

--- task02_game.py.py
+++ task02_game.py.py
@@ -4,28 +4,28 @@
 import random
 
 def main():
-    print("Ласкаво просимо до гри!")
-    print("Виберіть, камінь (1), ножиці (2) чи папір (3):")
+    print("Welcome to the game!")
+    print("Choose rock (1), scissors (2) or paper (3):")
 
-    user_choice = input("Введіть 1, 2, 3: ")
+    user_choice = input("Enter 1, 2, 3: ")
     user_choice = user_choice.strip()
 
     if user_choice not in ('1', '2', '3'):
-        print("Невірний вибір. Спробуйте ще раз.")
+        print("Wrong choice. Try again.")
         return
 
     user_choice = int(user_choice)
     computer_choice = random.randint(1, 3)
 
-    print("Ваш вибір:", user_choice)
-    print("Вибір комп'ютера:", computer_choice)
+    print("Your choice:", user_choice)
+    print("Choosing a computer:", computer_choice)
 
     if user_choice == computer_choice:
-        print("Нічия!")
+        print("Draw!")
     elif (user_choice == 1 and computer_choice == 2) or (user_choice == 2 and computer_choice == 3) or (user_choice == 3 and computer_choice == 1):
-        print("Ви виграли!")
+        print("You won!")
     else:
-        print("Комп'ютер виграв!")
+        print("The computer won!")
 
 if __name__ == "__main__":
     main()
